70cfccd66556d4cfbc477e6b3988929ef3b8e9b7
jordi sanchez <jordisanchez@macjordi.local>
2014-10-08 13:27:50 +0200

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by jordi sanchez on 08/10/14.
+//  Copyright (c) 2014 teleOficina Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by jordi sanchez on 08/10/14.
+//  Copyright (c) 2014 teleOficina Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
