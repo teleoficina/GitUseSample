d1c41bbe71bd9ddee8a8baf46075f033a0141137
jordi sanchez <jordisanchez@macjordi.local>
2014-10-08 16:53:17 +0200

    20141008@1653 a fusionar al trunk

--- GitUseExample/Test20141008ViewController.h
+++ GitUseExample/Test20141008ViewController.h
@@ -5,6 +5,7 @@
 //  Created by jordi sanchez on 08/10/14.
 //  Copyright (c) 2014 teleOficina Services. All rights reserved.
 //
+// esto lo quiero subir de new-branch al trunk
 
 #import <UIKit/UIKit.h>
 
